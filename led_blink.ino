86a89697422bdde0eb137450b5ed5877f4091864
pallavimahajan <pallavi.mahajan@os3infotech.com>
2017-10-07 17:14:33 +0530

    7

--- led_blink.ino
+++ led_blink.ino
@@ -2,13 +2,13 @@
 void setup()
 {
  // initialize digital pin 0 as an output.
-pinMode(0, OUTPUT);
+pinMode(2, OUTPUT);
  }
 // the loop function runs over and over again forever
 void loop()
 {
-digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
 delay(500);              // wait for a second
-digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
+digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
 delay(500);              // wait for a second
 }
